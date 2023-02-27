f8ec63da8d51cfa80edb10075e07736020b7b7c9
Swapnil De Sarkar <desarkar.swapnil.raj@gmail.com>
2023-02-28 05:04:48 +0530

    improved accuracy and added more pictures

--- img.py
+++ img.py
@@ -14,12 +14,11 @@ if file_path:
 
     face_cascade = cv2.CascadeClassifier('FD/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml')
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=2)
+    faces = face_cascade.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=5)
 
 
     for (x,y,w,h) in faces:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 0), 1)
-
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 256, 0), 1)
     cv2.imshow('Face Detection',img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
