7544b2de58deb4a9b98ccc13803b1f69cdaed343
Swapnil De Sarkar <desarkar.swapnil.raj@gmail.com>
2023-02-28 05:04:39 +0530

    improved accuracy and added more pictures

--- cam.py
+++ cam.py
@@ -11,7 +11,7 @@ while True:
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
     
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 0), 1)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 256, 0), 1)
     cv2.imshow('Face Detection', frame)
     
     if cv2.waitKey(1) == ord('q'):
